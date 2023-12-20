161f1c3a41f02aba704088d42d66c447636fd566
Andrey Melnikov <vafilor@gmail.com>
2023-12-20 13:29:05 -0800

    Add missing types. Update image creatIcon logic for heic files

--- src/server/image.ts
+++ src/server/image.ts
@@ -1,22 +1,19 @@
 import { extname } from "node:path";
 
-import convert from "heic-convert";
 import sharp from "sharp";
+import decode from 'heic-decode';
 
-import { readFile, writeFile } from "node:fs/promises";
+import { readFile } from "node:fs/promises";
 
 export async function createIcon(inputPath: string, outputPath: string, width: number, height: number): Promise<void> {
     const ext = extname(inputPath).toLowerCase();
 
     if (ext === ".heic") {
         const inputBuffer = await readFile(inputPath);
-        const outputBuffer = await convert({
-            buffer: inputBuffer,
-            format: 'JPEG',
-            quality: 1
-        });
+        const { data, width: imageWidth, height: imageHeight } = await decode({ buffer: inputBuffer });
 
-        await writeFile(outputPath, Buffer.from(outputBuffer));
+        // TODO 4 is a guess - need to validate
+        await sharp(data, { raw: { width: imageWidth, height: imageHeight, channels: 4 } }).resize(width, height).jpeg().toFile(outputPath);
     } else {
         await sharp(inputPath).resize(width, height).jpeg().toFile(outputPath);
     }
